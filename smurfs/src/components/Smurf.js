14c556775baee3b1492a9dad71ee1b10f2e0f922
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-02-09 14:00:08 -0600

    Added styling to Smurf info cards

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Name = styled.div`
+  height: 35px;
+  width: 200px;
+  border: 1px solid black;
+`
+const InfoCard = styled.div`
+  width: 200px;
+  height: 150px;
+  border: 1px solid blue;
+`
 
 class Smurf extends React.Component {
   constructor(props){
@@ -15,10 +27,12 @@ class Smurf extends React.Component {
   render(){
     return (
       <div>
-        <div>Name: {this.props.smurf.name}</div>
-        <div>Age: {this.props.smurf.age}</div>
-        <div>Height: {this.props.smurf.height}</div>
-        <button onClick={e => this.props.deleteSmurf(this.props.smurf.id)}>Delete Smurf</button>
+        <Name onClick={this.openCard}>{this.props.smurf.name}</Name>
+        {this.state.infoCardToggle ? <InfoCard>
+          <div>Age: {this.props.smurf.age}</div>
+          <div>Height: {this.props.smurf.height}</div>
+          <button onClick={e => this.props.deleteSmurf(this.props.smurf.id)}>Delete Smurf</button>
+        </InfoCard> : null}
       </div>
     );
   }
